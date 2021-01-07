97d82a5a03cd3b17fff078e145aca2344334e11e
Gaby-Fraga <gabyfragamoreiramedeiros@gmail.com>
2021-01-06 21:04:39 -0300

    aula de audios multiplos

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -5,8 +5,15 @@ public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 50f;
     [SerializeField] float mainThrust = 400f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State {Alive, Dying, Transcending};
+    State state = State.Alive;
     
     // Start is called before the first frame update
     void Start()
@@ -18,44 +25,79 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if(state == State.Alive){
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
      private void OnCollisionEnter(Collision collision) {
+
+        if(state != State.Alive){
+            return;
+        }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 print("Ok");
                 break;
             case "Finish":
-                print("Finish");
-                SceneManager.LoadScene(1);
+                StartSuccessSequence();
                 break;
             default:
-                print("Dead");
-                SceneManager.LoadScene(0);
+                StartDeathSequence();
                 break;
         }
     }
-    private void Thrust()
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextLevel", 1f);
+    }
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", 1f);
+    }
+
+
+    private void LoadFirstLevel(){
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel(){
+        SceneManager.LoadScene(1);
+    }
+    private void RespondToThrustInput()
     {
         float rotationThisFrame = mainThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * rotationThisFrame);
-            if(!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust(rotationThisFrame);
         }
         else
         {
-            audioSource.Pause();
+            audioSource.Stop();
         }
     }
-    private void Rotate(){
+
+    private void ApplyThrust(float rotationThisFrame)
+    {
+        rigidBody.AddRelativeForce(Vector3.up * rotationThisFrame);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput(){
 
         rigidBody.freezeRotation = true;
         
