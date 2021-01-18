ae82df9351d1b2d0989313c4f690ae3e2c3cfae6
Gaby-Fraga <gabyfragamoreiramedeiros@gmail.com>
2021-01-18 20:25:28 -0300

    luzes

--- 3_Project_Boost/Assets/Oscilator.cs
+++ 3_Project_Boost/Assets/Oscilator.cs
@@ -16,7 +16,7 @@ public class Oscilator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        movementVector.x = 30;
+        movementVector.x = 55;
         startingPos = transform.position;   
     }
 
