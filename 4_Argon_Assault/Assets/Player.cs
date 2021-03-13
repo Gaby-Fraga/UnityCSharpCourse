a8d025f726739a0fee0a2b4aaf3f0c802639a830
Gaby-Fraga <gabyfragamoreiramedeiros@gmail.com>
2021-03-13 14:37:30 -0300

    nave move para os lados limitada pela camera

---
+++ 4_Argon_Assault/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 400;
+    [Tooltip("In m")] [SerializeField] float xRange = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = Mathf.Clamp(transform.position.x + xOffset, -xRange, xRange);
+
+        transform.localPosition = new Vector3(rawNewXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
